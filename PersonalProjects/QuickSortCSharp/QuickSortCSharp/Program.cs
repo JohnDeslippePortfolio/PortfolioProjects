685ab20d1fb513e7d51fd2811aa75b5aa8b9ba76
JohnDeslippePortfolio <137830980+JohnDeslippePortfolio@users.noreply.github.com>
2023-07-13 10:59:21 -0400

    Add files via upload

---
+++ PersonalProjects/QuickSortCSharp/QuickSortCSharp/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace QuickSortCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            Random rand = new Random();
+
+            Console.Write("Enter how many random numbers to generate: ");
+            int userIn = Int32.Parse(Console.ReadLine()!);
+
+            for (int i = 0; i < userIn; i++)
+            {
+                list.Add(rand.Next(0, 10));
+            }
+
+            Console.WriteLine("Unsorted List: ");
+            foreach(int i in list)
+            {
+                Console.Write(i + " ");
+            }
+
+            QuickSort.Sort(list);
+            Console.WriteLine("Sorted List: ");
+            foreach (int i in list)
+            {
+                Console.Write(i + " "); 
+            }
+        }
+    }
+}
---
+++ PersonalProjects/QuickSortCSharp/QuickSortCSharp/QuickSort.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSortCSharp
+{
+    public class QuickSort
+    {
+        public static void Sort(List<int> userList)
+        {
+           QSort(userList, 0 ,userList.Count() - 1);
+
+        }
+
+        static void QSort(List<int> userList, int start, int end)
+        {
+            if(start >= end || start < 0)
+            {
+                return;
+            }
+            
+            int currentPartition = Partition(userList, start, end);
+
+            QSort(userList, currentPartition + 1, end);
+            QSort(userList, start, currentPartition-1);
+        }
+
+        static void Swap(List<int> userList, int firstIndex, int secondIndex)
+        {
+            int temp = userList[firstIndex];
+            userList[firstIndex] = userList[secondIndex];
+            userList[secondIndex] = temp;
+        }
+
+        static int Partition(List<int> userList, int start, int end) 
+        {
+            int pivot = userList[end];
+            int finalPivPosition = start - 1;
+
+            for(int i = start; i < end; i++) 
+            {
+                if (userList[i] <= pivot)
+                {
+                    finalPivPosition++;
+                    Swap(userList, i, finalPivPosition);
+                }
+            }
+
+            finalPivPosition++;
+            Swap(userList, finalPivPosition, end);
+            return finalPivPosition;
+
+
+        }
+    }
+}
