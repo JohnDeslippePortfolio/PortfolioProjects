72312c4490694194f48d9e2287d80d160c0971fa
JohnDeslippePortfolio <137830980+JohnDeslippePortfolio@users.noreply.github.com>
2023-07-13 10:58:42 -0400

    Add files via upload

---
+++ PersonalProjects/MergeSortCSharp/MergeSortCSharp/MergeSort.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeSortCSharp
+{
+    /*
+     * Merge Sort Algorithm:
+     * Given an array, split the array into subarrays until they are a single element,
+     * then as rememrge the single elements, sorting them as they are merged back into a single array
+     */
+    public class MergeSort
+    {
+   
+        public static List<int> Sort(List<int> origList)
+        {
+            if(origList.Count > 1)
+            {
+                List<int> lhList = Split (origList);
+
+                lhList = Sort(lhList);
+                origList = Sort(origList);
+
+                return Merge(lhList, origList);
+            }
+            return origList;
+            
+        }
+
+        static List<int> Split(List<int> inList)
+        {
+            List<int> outList = new List<int>();
+            int midPoint = inList.Count / 2;
+
+            for(int i = 0; i < midPoint; i++)
+            {
+                outList.Add(inList[i]);
+            }
+            inList.RemoveRange(0, midPoint);
+
+            return outList;
+        }
+
+        static List<int> Merge(List<int> lhList, List<int> rhList) 
+        {
+            int lIndex = 0;
+            int rIndex = 0;
+
+            List<int> mergedList= new List<int>();
+            while(lIndex < lhList.Count && rIndex < rhList.Count) 
+            {
+                if (lhList[lIndex] < rhList[rIndex])
+                {
+                    mergedList.Add(lhList[lIndex]);
+                    lIndex++;
+                }
+                else
+                {
+                    mergedList.Add(rhList[rIndex]);
+                    rIndex++;
+                }
+            }
+
+            for (; lIndex < lhList.Count; lIndex++)
+            {
+                mergedList.Add(lhList[lIndex]);
+            }
+
+            for(;rIndex < rhList.Count; rIndex++)
+            {
+                mergedList.Add(rhList[rIndex]);
+            }
+
+            return mergedList;
+        }
+
+    }
+}
---
+++ PersonalProjects/MergeSortCSharp/MergeSortCSharp/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace MergeSortCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> intList = new List<int>();
+            Random rGen = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                intList.Add(rGen.Next(0, 10));
+            }
+            Console.WriteLine("Before Sort:");
+            foreach (int i in intList)
+            {
+                Console.Write(i + " ");
+            }
+
+            intList = MergeSort.Sort(intList);
+            Console.WriteLine("After Sort:");
+            foreach(int i in intList)
+            {
+                Console.Write(i + " "); 
+            }
+        }
+    }
+}
